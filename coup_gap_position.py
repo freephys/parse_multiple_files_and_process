e2f5cde4ea28eb99ce032dce41c4293d5c17dc7f
John Anderson <sontek@gmail.com>
2014-05-13 10:40:54 -0500

    modify to create scatter plot of solvent positions in diatom-solvent plane

--- coup_gap_position.py
+++ coup_gap_position.py
@@ -9,8 +9,8 @@ import matplotlib.pyplot as plt
 # import matplotlib.animation as animation
 # from mpl_toolkits.mplot3d import Axes3D
 # from mpl_toolkits.mplot3d.art3d import juggle_axes
-import time
 import matplotlib.cm as cm
+from relative_x_y import *
 # fig = plt.figure()
 # ax = fig.add_axes([0, 0, 1, 1], projection='3d')
 # FLOOR = -15
@@ -41,7 +41,46 @@ def data_stream(target):
 		for (b1,b2,b3) in izip(read_in_chunk(coup),read_in_chunk(gap),read_in_chunk(xyz,503)):
                      target.send([b1,b2,b3])
 
+def data_stream_plot(target):
+	with open(r'./coup_xy_relative_with_value.out') as coup:
+		with open(r'./gap_xy_relative_with_value') as gap:
+			for (b1,b2) in izip(read_in_chunk(coup),read_in_chunk(gap)):
+				target.send([b1,b2])
+
 distance_x_y = lambda x : (((x[0]-x[1])**2).sum())**0.5
+
+@coroutine
+def plot_coup_gap():
+	"""@todo: Docstring for plot_coup_gap.
+
+	:plot_data: @todo
+	:returns: @todo
+
+	"""
+	time = 0
+	while True:
+		[coup,gap] = (yield)
+		coup_array = np.asarray([x.split() for x in coup])
+		gap_array = np.asarray([x.split() for x in gap])
+#		plt.xlim((-10,60))
+#		plt.ylim((-10,60))
+		ax1 = plt.subplot(121)
+		ax1.scatter(coup_array[:,0],coup_array[:,1])
+		ax1.set_title('coup data')
+		ax1.set_xlim([-10,60])
+		ax1.set_ylim([-10,60])
+		ax2 = plt.subplot(122)
+		ax2.scatter(gap_array[:,0],gap_array[:,1])
+		ax2.set_title('gap_data')
+		ax2.set_xlim([-10,60])
+		ax2.set_ylim([-10,60])
+		plt.suptitle('Frame'+str(time))
+		plt.savefig('timestep'+str(time)+'.png')
+		time = time + 1
+		plt.clf()
+    
+
+
 
 @coroutine
 def filter(sink):
@@ -49,70 +88,41 @@ def filter(sink):
     while True:
         [b1,b2,b3] = (yield)
         # print b1,b2,b3
-        coup_rank = [x.split()[1] for x in b1]
-        gap_rank = [x.split()[1] for x in b2]
+	t,coup_rank,coup_value = zip(*[x.split() for x in b1])
+        t,gap_rank,gap_value = zip(*[x.split() for x in b2])
         xyz_list = [x.split() for x in b3]
-        xyz_list.pop(0) # remove first two text header
+# remove first two text header
+        xyz_list.pop(0) 
         del xyz_list[0]
+#remove the first column of atom types such as Ar etc
         map(methodcaller("pop",0),xyz_list)
+#convert string to float type
         xyz_list = [map(float,xyz) for xyz in xyz_list]
-	from IPython.core.debugger import Tracer; breakpoint1 = Tracer()
+#point1 and point2 are positions of diatoms
 	point1 = xyz_list[0]
 	point2 = xyz_list[1]
-	#print point1,point2
 	zero_point = [(x+y)/2 for (x,y) in zip(point1,point2)] 
-	#print zero_point
-        #print coup_rank,gap_rank
         coup_rank = map(int,coup_rank)
         gap_rank = map(int,gap_rank)
-        #print coup_rank,gap_rank
-        xyz_list_gap = [xyz_list[x-1] for x in gap_rank]
-        xyz_list_coup = [xyz_list[x-1] for x in coup_rank]
-        #xyz_list_gap.insert(0,xyz_list[0])
-        #xyz_list_coup.insert(0,xyz_list[0])
-        #print coup_rank,gap_rank,xyz_list_gap,xyz_list_coup
-        in_coup_not_gap_set = set(coup_rank).difference(set(gap_rank))
-        in_gap_not_coup_set = set(gap_rank).difference(set(coup_rank))
-        #in_coup_not_gap_list.append(list(in_coup_not_gap_set))
-        #in_gap_not_coup_list.append(list(in_gap_not_coup_set))
-        #print in_coup_not_gap_list,  in_gap_not_coup_list
-        #gap_data = np.transpose(np.array(xyz_list_gap)-np.array(zero_point))
-        #coup_data = np.transpose(np.array(xyz_list_coup)-np.array(zero_point))
-        gap_data = np.transpose(np.array(xyz_list_gap)) #raw positions
-        coup_data = np.transpose(np.array(xyz_list_coup))
-        sink.send([in_coup_not_gap_set,in_gap_not_coup_set,gap_data,coup_data])
+        xyz_list_gap = [xyz_list[x] for x in gap_rank]
+        xyz_list_coup = [xyz_list[x] for x in coup_rank]
+#relative xy positions of Ar atom to diatoms in the same plane	
+	xy_relative_gap = [relative_position_within_three_point(x, point1, point2) for x in xyz_list_gap]
+	xy_relative_gap_with_value = merge_two_tuple(zip(xy_relative_gap,gap_value))
+	xy_relative_coup = [relative_position_within_three_point(x, point1, point2) for x in xyz_list_coup]
+	xy_relative_coup_with_value = merge_two_tuple(zip(xy_relative_coup,coup_value))
+	sink.send([xy_relative_coup_with_value,xy_relative_gap_with_value])
 
 @coroutine
 def sink():
-    in_coup_not_gap_list=[]
-    in_gap_not_coup_list=[]
-    time = 0.0
-    coup_unique_file = open('./coup_unique.out','w')
-    gap_unique_file = open('./gap_unique.out','w')
+    coup_xy_relative_with_value_file = open('./coup_xy_relative_with_value.out', 'w')
+    gap_xy_relative_with_value_file = open('./gap_xy_relative_with_value', 'w')
     while True:
-    	[coup_unique,gap_unique,gap_data,coup_data] = (yield)
-        in_coup_not_gap_list.append(list(coup_unique))
-        in_gap_not_coup_list.append(list(gap_unique))
-        #print coup_unique 
-	#print gap_unique
-	#map(str,coup_unique)
-	#map(str,gap_unique)
-	#coup_unique_file.write(', '.join(coup_unique)+"\n"+str(time)+"\n")
-	#gap_unique_file.write(', '.join(gap_unique)+"\n"+str(time)+"\n")
-	coup_unique_file.write(' '.join(str(x) for x in coup_unique)+"\n"+str(time)+"\n")
-	gap_unique_file.write(' '.join(str(x) for x in gap_unique)+"\n"+str(time)+"\n")
-        #plt.scatter(coup_data[0],coup_data[1],color=next(colors))
-        ax1.scatter(coup_data[0],coup_data[1])
-	ax2.scatter(gap_data[0],gap_data[1])
-        plt.savefig('./gap_coup_rank.png')
-	time = time+0.1
-	#plt.draw()
-	
-	#update.send(coup_data)	
-	#plt.clf()
-	#print gap_data
-	#print coup_data
-	#print "finish one loop"
+	[coup_xy_relative_with_value, gap_xy_relative_with_value] = (yield)
+	for t in coup_xy_relative_with_value:
+		coup_xy_relative_with_value_file.write(' '.join(str(x) for x in t)+'\n')
+	for t in gap_xy_relative_with_value:
+		gap_xy_relative_with_value_file.write(' '.join(str(x) for x in t)+'\n')
 
 @coroutine
 def animate():
@@ -120,7 +130,14 @@ def animate():
           data = (yield)
           scat._offsets3d = juggle_axes( data[0] , data[1] , data[2],'z' )
           #ax.view_init(30,0)
+def merge_two_tuple(list_of_tuples):
+	"""@todo: Docstring for merge_two_tuple within a list of tuple.
 
+	:list_of_tuples: @todo
+	:returns: @todo
+
+	"""
+        return [tuple(j for i in x for j in (i if isinstance(i,tuple) else (i,))) for x in list_of_tuples] 
 if __name__ == '__main__':
     # a1 = AnimatedScatter()
     # data_stream(filter(sink(animate())))
@@ -135,6 +152,7 @@ if __name__ == '__main__':
     # ax.set_zlim3d(FLOOR,CEILING)
     # scat = ax.scatter([],[],[],animated=True)
     data_stream(filter(sink()))
+    data_stream_plot(plot_coup_gap())
     # instantiate the animator.
     # anim = animation.FuncAnimation(fig, animate,interval=30, blit=True)
     # fig.show()
